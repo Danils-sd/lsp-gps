57a8b9433419879988a29c13876d60496ab2c4b5
Danils-sd <hlebushek.sd@gmail.com>
2024-11-17 00:58:27 +0300

    label

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -6,13 +6,39 @@ function Main() {
         <div className="main-container">
             <img src="./imgs/olegback.jpg" className={classCont + "--img"}/>
             <div className={classCont + "--content"}>
-                <img src="./imgs/logo(lsp).png" className={classCont + "--img-lsp"}/>
+                <div className={classCont + "--img-lsp-cont"}>
+                    <img src="./imgs/logo(lsp).png" className={classCont + "--img-lsp"}/>
+                </div>
                 <h1 className={classCont + "--h1"}>29 НОЯБРЯ</h1>
                 <h2 className={classCont + "--h2"}>НОЧНОЕ ВЫСТУПЛЕНИЕ | НАЧАЛО: 23:00</h2>
                 <div className={classCont + "--dop-cont"}>
                     <img src="./imgs/18.png" className={classCont + "--dop-cont" + "--img"}/>
                     <img src="./imgs/local.png" className={classCont + "--dop-cont" + "--img"}/>
                     <img src="./imgs/sagrado.png" className={classCont + "--dop-cont" + "--img"}/>
+                </div>
+            </div>
+            <div className={classCont + "--shadow"}>
+                <div className={classCont + "--shadow--row"}>
+                    <div className={classCont + "--shadow--cub"}></div>
+                    <div className={classCont + "--shadow--cub"}></div>
+                </div>
+                <div className={classCont + "--shadow--row"}>
+                    <div className={classCont + "--shadow--cub"}></div>
+                </div>
+                <div className={classCont + "--shadow--row"}>
+                    <div className={classCont + "--shadow--cub"}></div>
+                    <div className={classCont + "--shadow--cub"}></div>
+                </div>
+                <div className={classCont + "--shadow--row"}>
+                    <div className={classCont + "--shadow--cub"}></div>
+                    <div className={classCont + "--shadow--cub"}></div>
+                    <div className={classCont + "--shadow--cub"}></div>
+                </div>
+                <div className={classCont + "--shadow--row"}>
+                    <div className={classCont + "--shadow--cub"}></div>
+                    <div className={classCont + "--shadow--cub"}></div>
+                    <div className={classCont + "--shadow--cub"}></div>
+                    <div className={classCont + "--shadow--cub"}></div>
                 </div>
             </div>
         </div>
